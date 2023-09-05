b15a62fcaed2b29f31250218309f72a0f899ab8f
Larissa Vianna Cavalcante <larissavcavalcante@outlook.com>
2023-09-04 21:03:32 -0300

    projeto com tres tabelas banco de dados

---
+++ prisma/migrations/20230904231927_create_table_subjects/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "subjects" (
+    "id" TEXT NOT NULL,
+    "course_name" TEXT NOT NULL,
+    "workload" TEXT NOT NULL,
+
+    CONSTRAINT "subjects_pkey" PRIMARY KEY ("id")
+);
---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import fastify from "fastify";
+
+import { PrismaClient } from '@prisma/client';
+
+const app = fastify();
+
+const prisma = new PrismaClient();
+
+app.listen({
+    port:3333,
+}).then(() => {
+    console.log('HTTP Server Running On Port 3333')
+});
+
+app.get('/students', async() => {
+    const students = await prisma.students.findMany();
+    return students;
+})
+
+app.get('/teachers', async() => {
+    const teachers = await prisma.teachers.findMany();
+    return teachers;
+})
+
+app.get('/subjects', async() => {
+    const subjects = await prisma.subjects.findMany();
+    return subjects;
+})
